a4df4d2e753666f7824fa7f3aeda7f406be1b2ad
Valera <valery.stavitsky@gmail.com>
2019-10-16 00:31:37 +0300

    update example

---
+++ example/index.php
@@ -0,0 +1,141 @@
+<?php
+/**
+ * phpWhois Example
+ *
+ * This class supposed to be instantiated for using the phpWhois library
+ *
+ * @license http://www.gnu.org/licenses/gpl-2.0.html GNU General Public License, version 2
+ * @license
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * @link http://phpwhois.pw
+ * @copyright Copyright (C)1999,2005 easyDNS Technologies Inc. & Mark Jeftovic
+ * @copyright Maintained by David Saez
+ * @copyright Copyright (c) 2014 Dmitry Lukashin
+ * @copyright Copyright (c) 2019 Valeriy Stavitskiy
+ */
+
+header('Content-Type: text/html; charset=UTF-8');
+
+if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    require_once __DIR__ . '/../vendor/autoload.php';
+}
+
+use phpWhois\Whois;
+use phpWhois\Utils;
+
+$whois = new Whois();
+
+$data_out = [
+    'self' => $_SERVER['PHP_SELF'] ?? '',
+    'ver' => $whois->codeVersion ?? '',
+];
+
+if (isset($_GET['query'])) {
+
+    $get_params['query'] = filter_input(INPUT_GET, 'query');
+    $get_params['output'] = (!empty($_GET['output'])) ? filter_input(INPUT_GET, 'output') : '';
+    $get_params['fast'] = (!empty($_GET['fast'])) ? filter_input(INPUT_GET, 'fast') : '';
+
+
+    // Set to true if you want to allow proxy requests
+    $allowproxy = false;
+
+    // get faster but less acurate results
+    $whois->deep_whois = empty($get_params['fast']);
+
+
+    // To use special whois servers (see README)
+    //$whois->UseServer('uk','whois.nic.uk:1043?{hname} {ip} {query}');
+    //$whois->UseServer('au','whois-check.ausregistry.net.au');
+
+    // Comment the following line to disable support for non ICANN tld's
+    $whois->non_icann = true;
+
+    $result = $whois->Lookup($get_params['query']);
+
+    $data_out['query'] = $get_params['query'] ?? '';
+    $data_out['get_params'] = $get_params ?? [];
+
+    $winfo = '';
+
+    switch ($get_params['output']) {
+        case 'object':
+            if ($whois->query['status'] < 0) {
+                $winfo = implode($whois->query['errstr'], "\n<br />");
+            } else {
+                $utils = new Utils;
+                $winfo = $utils->showObject($result);
+            }
+            break;
+
+        case 'nice':
+            if (!empty($result['rawdata'])) {
+                $utils = new Utils;
+                $winfo = $utils->showHTML($result);
+            } else {
+                if (isset($whois->query['errstr']))
+                    $winfo = implode($whois->query['errstr'], "\n<br />");
+                else
+                    $winfo = 'Unexpected error';
+            }
+            break;
+
+        case 'proxy':
+            if ($allowproxy) {
+                ob_start();
+                echo "<pre>";
+                print_r(serialize($result));
+                echo "</pre>";
+                $winfo = ob_get_contents();
+                ob_end_clean();
+            }
+            break;
+
+        default:
+            if (!empty($result['rawdata'])) {
+                $winfo = '<pre>' . implode($result['rawdata'], "\n") . '</pre>';
+            } else {
+                $winfo = implode($whois->query['errstr'], "\n<br />");
+            }
+    }
+
+
+    $data_out['winfo'] = $winfo ?? '';
+}
+
+
+exit(view($data_out, 'template.php'));
+
+//-------------------------------------------------------------------------
+
+
+function view(array $data, string $tpl): string {
+    $content = '';
+
+    if (file_exists($tpl)) {
+        ob_start();
+
+        if (!empty($data)) {
+            extract($data);
+        }
+
+        include $tpl;
+        $content = ob_get_contents();
+        ob_end_clean();
+    }
+
+    return $content;
+}
---
+++ example/template.php
@@ -0,0 +1,123 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>whois.php -base classes to do whois queries with php</title>
+    <style type="text/css">
+        body {
+            overflow-x: hidden;
+            position: relative;
+        }
+
+        main {
+            width: 100%;
+            min-height: 200px;
+            padding: 10px;
+        }
+
+        p {
+            margin: 0;
+            padding: 5px 0;
+            line-height: 1.2;
+        }
+
+        .container {
+            width: 1200px;
+            margin: 0 auto;
+        }
+
+
+        h1 {
+            text-align: center;
+        }
+
+        form {
+            background-color: #55aaff;
+            padding: 10px;
+            position: relative;
+        }
+
+        form input {
+            margin: 5px;
+        }
+
+        form label,
+        form input[type="radio"],
+        form input[type="checkbox"] {
+            cursor: pointer;
+        }
+
+        form img {
+            border: 0 solid;
+        }
+
+        .logo-form {
+            position: absolute;
+            bottom: 10px;
+            right: 10px;
+        }
+
+        footer {
+            width: 100%;
+            text-align: center;
+            padding: 10px;
+        }
+    </style>
+</head>
+<body>
+
+<header>
+    <div class="container">
+        <h1>phpWhois <?= $ver ?? '' ?> - base class to do whois queries with php</h1>
+        <form method="get" action="<?= $self ?? '' ?>">
+            <div>
+                <b>Enter any domain name, ip address or AS handle you would like to query whois for</b>
+                <br/><br/>
+                <input name="query" value="<?= $get_params['query'] ?? '' ?>"/>
+                <input type="submit" value="Whois"/><br/>
+            </div>
+            <div>
+                <div>
+                    <input id="output_normal" type="radio" name="output" value="normal" <?= $get_params['output'] == "normal" ? 'checked="checked"' : '' ?> />
+                    <label for="output_normal">Show me regular output</label>
+                </div>
+                <div>
+                    <input id="output_nice" type="radio" name="output" value="nice" <?= $get_params['output'] == "nice" ? 'checked="checked"' : '' ?> />
+                    <label for="output_nice">Show me HTMLized output</label>
+                </div>
+                <div>
+                    <input id="output_object" type="radio" name="output" value="object"<?= $get_params['output'] == "object" ? 'checked="checked"' : '' ?> />
+                    <label for="output_object">Show me the returned PHP object</label>
+                </div>
+                <div>
+                    <input id="fast" type="checkbox" name="fast" value="1" <?= $get_params['fast'] ? 'checked="checked"' : '' ?> />
+                    <label for="fast">Fast lookup</label>
+                </div>
+            </div>
+
+            <div class="logo-form"><a href="http://www.phpwhois.org" title="phpWhois web page"><img src="whois.icon.png" alt="phpWhois web page"/></a></div>
+        </form>
+    </div>
+</header>
+
+
+<main>
+    <div class="container">
+        <?php if (!empty($query)) { ?>
+            <div><strong>Results for <?= $query ?> :</strong></div>
+            <blockquote>
+                <?= $winfo ?? '' ?>
+            </blockquote>
+        <?php } ?>
+    </div>
+</main>
+
+<footer>
+    <div class="container">
+        <p>&copy; 1999 - 2011 <a href="http://www.easydns.com/">easyDNS Technologies Inc.</a> &amp; <a href="http://mark.jeftovic.net/">Mark Jeftovic</a></p>
+        <p>Now maintained and hosted by David Saez at <a href="http://www.ols.es">OLS 20000</a></p>
+        <p>Placed under the GPL. See the LICENSE file in the distribution.</p>
+    </div>
+</footer>
+
+</body>
+</html>
